47200829fb93c944d89314920857cb99e92f4027
wangko27 <wangkun@nuls.io>
2018-09-26 13:58:51 +0800

    LevelDB Test

--- src/test/java/io/batao/nuls/demo/test/AppTest.java
+++ src/test/java/io/batao/nuls/demo/test/AppTest.java
@@ -2,13 +2,40 @@ package io.batao.nuls.demo.test;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by wangkun23 on 2018/9/6.
  */
 public class AppTest {
+    static final Logger logger = LoggerFactory.getLogger(AppTest.class);
 
-    public static void main(String[] args) {
-        String json = "[{\"event\":\"VoteCreateEvent\",\"payload\":{\"voteId\":\"1\",\"title\":\"wql123\",\"desc\":\"321\",\"items\":\"[{id: 1, content: \\\"123\\\"}, {id: 2, content: \\\"dsf\\\"}, {id: 3, content: \\\"23\\\"}, {id: 4, content: \\\"124\\\"}, {id: 5, content: \\\"32\\\"}, {id: 6, content: \\\"3\\\"}]\"}}]";
-        JSON.parseArray(json);
+    /*private static DBService dbService;
+
+    @Before
+    public void init() {
+        dbService = new LevelDBServiceImpl();
     }
+
+    @Test
+    public void test() throws NulsException {
+        byte[] heightBytes = new VarInt(100).encode();
+        byte[] hashBytes = dbService.get(ProtocolStorageConstant.DB_NAME_BLOCK_HEADER_INDEX, heightBytes);
+        byte[] bytes = dbService.get(ProtocolStorageConstant.DB_NAME_BLOCK_HEADER, hashBytes);
+
+        BlockHeaderPo po = new BlockHeaderPo();
+        po.parse(bytes, 0);
+        logger.info("data ## {}",po);
+
+        NulsDigestData hash = new NulsDigestData();
+        try {
+            hash.parse(hashBytes, 0);
+        } catch (NulsException e) {
+            Log.error(e);
+        }
+        logger.info("data ## {}",hash);
+    }*/
 }
